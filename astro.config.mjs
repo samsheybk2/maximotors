209030efb27314c7698b323d51bf0081e82e924f
samsheybk2 <samireduardovasquezzambrano@gmail.com>
2026-06-08 01:40:38 -0400

    chore: use @astrojs/vercel adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://maximotors.pa',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [tailwind()],
 });
